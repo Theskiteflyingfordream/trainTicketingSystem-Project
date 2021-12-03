25266e5a95101529864444e6b2b2bbf3cf1404e4
sky <1922840103@qq.com>
2021-12-03 21:15:53 +0800

    SSMP Version

--- trainTicketingSystem/src/main/java/com/team/ticketing/mapper/train.java
+++
@@ -1,9 +0,0 @@
-package com.team.ticketing.mapper;
-
-/**
- * @ClassName train
- * @Description TODO
- * @DATE 2021/11/29 19:40
- */
-public class train {
-}
---
+++ trainTicketingSystem/src/test/java/Test01.java
@@ -0,0 +1,32 @@
+/*
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.team.ticketing.bean.Pmember;
+import com.team.ticketing.mapper.PmemberMapper;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+*/
+
+/**
+ * @ClassName Test01
+ * @Description TODO
+ * @DATE 2021/12/1 18:35
+ */
+public class Test01 {
+    /*
+    private ApplicationContext ioc =
+            new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    private PmemberMapper pmemberMapper =
+            ioc.getBean("pmemberMapper", PmemberMapper.class);
+
+    @Test
+    public void test01(){
+        pmemberMapper.selectList(null);
+        IPage<Pmember> ipage = pmemberMapper.selectPage(new Page<Pmember>(2,1), null);
+        System.out.println(ipage.getCurrent());
+    }
+    */
+
+}
